e72b44198911c98644b41133cf07e15793b811d9
Cyrus <cyrus.rumao26@gmail.com>
2025-03-21 16:03:24 +0530

    chamges

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,23 +1,22 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import bodyParser from "body-parser";
-// import authRoute from "./Routes/authRoute.js";
-// dotenv.config();
-// import { connectDB } from "./Models/db.js";
-// import prodRoute from "./Routes/prodRoute.js";
-// const app = express();
-// const PORT = process.env.PORT || 8080;
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import authRoute from "./Routes/authRoute.js";
+dotenv.config();
+import { connectDB } from "./Models/db.js";
+import prodRoute from "./Routes/prodRoute.js";
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// app.get("/hello", (req, res) => {
-//   res.send("world");
-// });
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(cors());
+app.get("/hello", (req, res) => {
+  res.send("world");
+});
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
-// connectDB();
-// app.use("/auth", authRoute);
-// app.use("/products", prodRoute);
-// export default createServer(app);
-
+connectDB();
+app.use("/auth", authRoute);
+app.use("/products", prodRoute);
+export default createServer(app);
